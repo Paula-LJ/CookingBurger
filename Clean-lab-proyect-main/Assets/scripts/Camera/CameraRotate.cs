26119a75e11a26459559062419d6aba366a0938e
Paula Liñán Jurado <paula.linan01@estudiant.upf.edu>
2021-05-11 21:53:08 +0200

    Rotacion corregida (supuestamente)

--- Clean-lab-proyect-main/Assets/scripts/Camera/CameraRotate.cs
+++ Clean-lab-proyect-main/Assets/scripts/Camera/CameraRotate.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class CameraRotate : MonoBehaviour
 {
     float time = 0.0f;
-    float timeChange = 5.0f;
+    
+    public float timeChange = 5.0f;
     public Camera cam;
     public Camera cam1;
+    
     private Vector3 NewPosition; // = new Vector3(49, 30, 72); 
     private Vector3 NewRotation; //= new Vector3(0, 180, 0); 
+    private bool moveCamera = false;
+    private Vector3 com_rot = new Vector3(0, -180, 0);
+
     // Start is called before the first frame update
     private Vector3 rota = new Vector3  (90,0,0);
     void Start()
@@ -23,13 +28,15 @@ public class CameraRotate : MonoBehaviour
     {
         time += Time.deltaTime;
         //Debug.Log(time);
-        Debug.Log(cam1.transform.eulerAngles);
+        //Debug.Log(cam1.transform.eulerAngles);
 
-        if (time >= timeChange && time <= timeChange+0.1f)
+        if (time >= timeChange && time <= timeChange+0.5f && moveCamera == false)
         {
+            moveCamera = true;
             cam.transform.position = NewPosition;
-           // if (cam.CompareTag("MainCamera"))
-                cam.transform.Rotate( NewRotation, Space.World);
+            if (cam.CompareTag("Cam1"))
+                cam.transform.Rotate(com_rot, Space.World);
+            cam.transform.Rotate( NewRotation, Space.World);
 
         }
     }
