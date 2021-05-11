4ef6a12cd61ae799582cae30a69e3a169ac032af
Patricia <patricia.martinez.cruz.pmn@gmail.com>
2021-05-11 21:06:03 +0100

    camera rotation

--- Clean-lab-proyect-main/Assets/scripts/Camera/CameraRotate.cs
+++ Clean-lab-proyect-main/Assets/scripts/Camera/CameraRotate.cs
@@ -5,22 +5,32 @@ using UnityEngine;
 public class CameraRotate : MonoBehaviour
 {
     float time = 0.0f;
-    public Camera cam; 
-    Vector3 pos3 = new Vector3(49, 30, 72); 
+    float timeChange = 5.0f;
+    public Camera cam;
+    public Camera cam1;
+    private Vector3 NewPosition; // = new Vector3(49, 30, 72); 
+    private Vector3 NewRotation; //= new Vector3(0, 180, 0); 
     // Start is called before the first frame update
+    private Vector3 rota = new Vector3  (90,0,0);
     void Start()
     {
-        
+        NewPosition = cam1.transform.position;
+        NewRotation = cam1.transform.eulerAngles - rota;
     }
 
     // Update is called once per frame
     void Update()
     {
         time += Time.deltaTime;
-        Debug.Log(time);
-        if (time >= 5.0f && time <= 5.5f)
+        //Debug.Log(time);
+        Debug.Log(cam1.transform.eulerAngles);
+
+        if (time >= timeChange && time <= timeChange+0.1f)
         {
-            cam.transform.position = pos3; 
+            cam.transform.position = NewPosition;
+           // if (cam.CompareTag("MainCamera"))
+                cam.transform.Rotate( NewRotation, Space.World);
+
         }
     }
 }
